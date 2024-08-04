6d40a3bca20023c058f79fca3364b6cf9b7437c7
logabaalan <115532100+logabaalan777@users.noreply.github.com>
2024-08-04 16:00:48 +0530

    Add files via upload

---
+++ github-demo/app.py
@@ -0,0 +1,19 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def main():
+    # Get user input
+    num = int(input("Enter a positive integer: "))
+    
+    if num < 0:
+        print("Factorial does not exist for negative numbers.")
+    else:
+        result = factorial(num)
+        print(f"The factorial of {num} is {result}")
+
+if __name__ == '__main__':
+    main()
+
